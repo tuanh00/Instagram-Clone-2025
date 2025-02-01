a59e2647cb4741cdf0f7450f4d6edfef6188050d
TuAnh <june265zz@gmail.com>
2025-02-01 17:32:06 -0500

    Initial Commit

---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by LaSalle on 2025-02-01.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
